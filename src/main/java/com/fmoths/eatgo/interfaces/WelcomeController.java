15b22440bcc5a18d66a484505b1b929891299230
kdpark <kdpark1623@gmail.com>
2020-08-03 22:45:14 +0900

    test welcome controller 생성

---
+++ src/main/java/com/fmoths/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.fmoths.eatgo.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello world!";
+    }
+}

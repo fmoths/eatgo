7a42d256697492897e5db0c60f86af95d9ebdacd
kdpark <kdpark1623@gmail.com>
2020-08-03 23:14:05 +0900

    C:/Program Files/Git/list API 생성

--- src/test/java/com/fmoths/eatgo/domain/RestaurantTests.java
+++ src/test/java/com/fmoths/eatgo/domain/RestaurantTests.java
@@ -8,14 +8,14 @@ class RestaurantTests {
 
     @Test
     public void creation(){
-        Restaurant restaurant = new Restaurant("Bob zip", "Seoul");
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
         assertEquals("Bob zip",restaurant.getName());
         assertEquals("Seoul",restaurant.getAddress());
     }
 
     @Test
     public void information(){
-        Restaurant restaurant = new Restaurant("Bob zip","Seoul");
+        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
         assertEquals("Bob zip in Seoul",restaurant.getInfomation());
     }
 }

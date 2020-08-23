b11d55167a74800ea461db39d83a5b4afe07e04b
kdpark <kdpark1623@gmail.com>
2020-08-23 11:37:10 +0900

    Review 생성기능 추가

---
+++ eatgo-api/src/main/java/com/fmoths/eatgo/interfaces/ReviewService.java
@@ -0,0 +1,12 @@
+package com.fmoths.eatgo.interfaces;
+
+import com.fmoths.eatgo.domain.Review;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    public void addReview(Review review){
+
+    }
+}
---
+++ eatgo-api/src/test/java/com/fmoths/eatgo/interfaces/ReviewControllerTests.java
@@ -0,0 +1,35 @@
+package com.fmoths.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ReviewController.class)
+class ReviewControllerTests {
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    private ReviewService reviewService;
+
+    @Test
+    public void create() throws Exception {
+        mvc.perform(post("/restaurants/1/reviews")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content("{\"name\":\"kdpak\",\"score\":3,\"description\":\"good!\"}"))
+            .andExpect(status().isCreated());
+
+
+        verify(reviewService).addReview(any());
+    }
+}
